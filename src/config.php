314ef88860b9dc4689fcb217f2a7b8ecf70b72f0
PAJEAN <pierre-antoine.jean@mines-ales.fr>
2021-12-09 09:17:45 +0100

    Pagination.

---
+++ src/config.php
@@ -0,0 +1,11 @@
+<?php
+    /* Importer la librairie TwitterAPIExchange.php */
+    require_once('TwitterAPIExchange.php');
+    /* Renseigner vos clés d'accés */
+    $settings = array(
+        "oauth_access_token" => "",
+        "oauth_access_token_secret" => "",
+        "consumer_key" => "",
+        "consumer_secret" => ""
+    );
+?>
