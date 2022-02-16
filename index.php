19b7448f2cccae740d20e79fafd289eb1dcdfade
PAJEAN <pierre-antoine.jean@mines-ales.fr>
2022-02-16 13:54:05 +0100

    Update 16-02-22.

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ require_once('TwitterAPIExchange.php');
 $settings = array(
     "oauth_access_token" => "",
     "oauth_access_token_secret" => "",
-    "consumer_key" => "",
-    "consumer_secret" => ""
+    "consumer_key" => "", // API key.
+    "consumer_secret" => "" // API secret key.
 );
 
 ?>
